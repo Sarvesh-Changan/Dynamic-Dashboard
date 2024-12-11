6962fccb2a096251a994ce6e9a85c440b1adbe05
Sarvesh <changansarvesh0@gmail.com>
2024-12-11 18:04:18 +0530

    first commit

---
+++ src/components/settings/Profile.jsx
@@ -0,0 +1,26 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+
+const Profile = () => {
+	return (
+		<SettingSection icon={User} title={"Profile"}>
+			<div className='flex flex-col sm:flex-row items-center mb-6'>
+				<img
+					src='https://lh3.googleusercontent.com/a/ACg8ocJrD6E2xFYPQjX8nUUEnZdN0QUVDdWYfR8gvmYZQk7aBxEhjeis=s360-c-no'
+					alt='Profile'
+					className='rounded-full w-20 h-20 object-cover mr-4'
+				/>
+
+				<div>
+					<h3 className='text-lg font-semibold text-gray-100'>John Doe</h3>
+					<p className='text-gray-400'>sarvesh.webdev@gmail.com</p>
+				</div>
+			</div>
+
+			<button className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto'>
+				Edit Profile
+			</button>
+		</SettingSection>
+	);
+};
+export default Profile;
